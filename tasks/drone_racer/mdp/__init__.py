d3f222394feaef488597ad4ecc7a7f8e38d02bf5
Boyang Hu <ssybh2@nottingham.edu.cn>
2026-09-21 18:01:55 +0800

    feat(control): expose Swift CTBR action through MDP

--- tasks/drone_racer/mdp/__init__.py
+++ tasks/drone_racer/mdp/__init__.py
@@ -9,6 +9,7 @@
 from isaaclab.envs.mdp import *  # noqa: F401, F403
 
 from .actions import *  # noqa: F401, F403
+from .swift_ctbr_action import *  # noqa: F401, F403
 from .commands import *  # noqa: F401, F403
 from .events import *  # noqa: F401, F403
 from .observations import *  # noqa: F401, F403
